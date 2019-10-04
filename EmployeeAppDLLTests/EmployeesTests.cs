76f690d6b82e825df916b83fd40cc49a67cc7124
oscarnjerenga <46794204+oscarnjerenga@users.noreply.github.com>
2019-10-04 10:51:34 +0300

    Add files via upload

---
+++ EmployeeAppDLLTests/EmployeesTests.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using EmployeeAppDLL;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeeAppDLL.Tests
+{
+    [TestClass()]
+    public class EmployeesTests
+    {
+        [TestMethod()]
+        public void EmployeesTest()
+        {
+            Employees loadfile = new Employees();
+            Console.WriteLine("File Successfully loaded");
+        }
+
+        [TestMethod()]
+        public void SalaryTest()
+        {
+            Employees loadfile = new Employees();
+            loadfile.getSalary();
+            Console.WriteLine("Salary Successfully deduced");
+        }
+    }
+}
